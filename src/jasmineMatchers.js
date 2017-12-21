01b1ab05ea70c31daf35e38d3382e5befeada132
Massimo Foti <massimo.foti@appway.com>
2017-12-21 20:31:01 +0100

    Added .toMatchDuckType() and .toBeReadonly()

--- src/jasmineMatchers.js
+++ src/jasmineMatchers.js
@@ -4,13 +4,103 @@ if(typeof(jQuery) === "undefined"){
 }
 
 /* istanbul ignore else */
-if(typeof(window.jasmineFixture) === "undefined"){
+if(typeof(window.jasmineMatchers) === "undefined"){
 	window.jasmineMatchers = {};
 }
+
+/**
+ * @typedef {Object} jasmineMatchers.result
+ *
+ * @property {Boolean} pass
+ * @property {String} [message]
+ */
 
 (function(){
 	"use strict";
 
 	jasmineMatchers.version = "0.1";
+
+	jasmineMatchers.toMatchDuckType = function(){
+		return {
+			/**
+			 * @param {Object} actual
+			 * @param {Object} duckType
+			 * @param {Boolean} [matchType] Define if we have to compare type of property. Default to true
+			 * @return {jasmineMatchers.result}
+			 */
+			compare: function(actual, duckType, matchType){
+				var result = {
+					pass: false
+				};
+				if(duckType === undefined){
+					result.message = "Please specify an instance of a duckType";
+					return result;
+				}
+				if(jQuery.type(actual) !== jQuery.type(duckType)){
+					result.message = "Type mismatch, comparing: " + jQuery.type(actual) + " vs " + jQuery.type(duckType);
+					return result;
+				}
+				if(matchType === undefined){
+					// By default we check for type
+					matchType = true;
+				}
+				for(var key in duckType){
+					if(duckType.hasOwnProperty(key) === true){
+						var duckProp = duckType[key];
+						if(actual.hasOwnProperty(key) === true){
+							if(matchType === true){
+								if(jQuery.type(duckProp) !== jQuery.type(actual[key])){
+									result.pass = false;
+									result.message = "Type of: ." + key + " does not match. Supposed to be: " + jQuery.type(duckProp);
+									return result;
+								}
+							}
+						}
+						else{
+							result.message = "The following duck property is missing: ." + key;
+							return result;
+						}
+					}
+				}
+
+				result.pass = true;
+				return result;
+			}
+		};
+	};
+
+	jasmineMatchers.toBeReadonly = function(){
+		return {
+			/**
+			 * @param {Object} parentObject
+			 * @param {String} property
+			 * @return {jasmineMatchers.result}
+			 */
+			compare: function(parentObject, property){
+				var result = {
+					pass: false
+				};
+				if(jQuery.type(property) !== "string"){
+					result.message = "Please specify the name of the property as string";
+					return result;
+				}
+				var desc = Object.getOwnPropertyDescriptor(parentObject, property);
+				if(desc === undefined){
+					result.message = "Unable to find property: " + property;
+					return result;
+				}
+				if(desc.writable === false){
+					result.pass = true;
+					return result;
+				}
+				result.message = "Property: " + property + " is not readonly";
+				return result;
+			}
+		};
+	};
+
+	jasmine.getEnv().beforeEach(function(){
+		jasmine.getEnv().addMatchers(jasmineMatchers);
+	});
 
 }());
--- test/spec/jasmineMatchers.Spec.js
+++ test/spec/jasmineMatchers.Spec.js
@@ -2,12 +2,8 @@ describe("jasmineMatchers", function(){
 
 	"use strict";
 
-	beforeEach(function(){
-
-
-	});
-
-	it("Requires jQuery in order to work", function(){
+	it("Requires Jasmine and jQuery in order to work", function(){
+		expect(jasmine).toBeDefined();
 		expect(jQuery).toBeDefined();
 	});
 
@@ -19,6 +15,152 @@ describe("jasmineMatchers", function(){
 		it("Reports the current version number", function(){
 			expect(jasmineMatchers.version).toBeDefined();
 		});
+	});
+
+	describe("Provides the following custom matchers for Jasmine:", function(){
+
+		describe(".toMatchDuckType()", function(){
+
+			var matcher;
+			beforeEach(function(){
+				matcher = jasmineMatchers.toMatchDuckType();
+			});
+
+			describe("Given an object and another object to be used as duckType:", function(){
+
+				describe("Matches if:", function(){
+
+					it("The compared object contain the same set of properties as the duckType one, even if values are different", function(){
+						var result = matcher.compare({name: "Ciccio"}, {name: "Duck"});
+						expect(result.pass).toBe(true);
+					});
+
+					it("The compared object contain properties not available inside the duckType one", function(){
+						var result = matcher.compare({name: "Ciccio"}, {});
+						expect(result.pass).toBe(true);
+					});
+
+					it("Neither the object or the duckType contain any own property", function(){
+						var result = matcher.compare({}, {});
+						expect(result.pass).toBe(true);
+					});
+
+				});
+
+				describe("Fails if:", function(){
+
+					it("The compared objects are of different types", function(){
+						var result = matcher.compare({}, []);
+						expect(result.pass).toBe(false);
+						expect(result.message).toBe("Type mismatch, comparing: object vs array");
+					});
+
+					it("The duckType object contain properties not available inside the compared one", function(){
+						var result = matcher.compare({}, {name: "Ciccio"});
+						expect(result.pass).toBe(false);
+						expect(result.message).toBe("The following duck property is missing: .name");
+					});
+
+					it("The two object contain the same property but it is of different types", function(){
+						var result = matcher.compare({name: []}, {name: "Ciccio"});
+						expect(result.pass).toBe(false);
+						expect(result.message).toBe("Type of: .name does not match. Supposed to be: string");
+					});
+
+					it("Only one argument is provided", function(){
+						var result = matcher.compare({});
+						expect(result.pass).toBe(false);
+						expect(result.message).toBe("Please specify an instance of a duckType");
+					});
+
+				});
+
+				describe("Accept a boolean as third, optional argument", function(){
+
+					it("If set to false, the type of properties is not compared", function(){
+						var result = matcher.compare({name: []}, {name: "Ciccio"}, false);
+						expect(result.pass).toBe(true);
+						expect(result.message).toBeUndefined();
+					});
+
+				});
+
+			});
+
+		});
+
+		describe(".toBeReadonly()", function(){
+
+			var matcher;
+			beforeEach(function(){
+				matcher = jasmineMatchers.toBeReadonly();
+			});
+
+			describe("Given an object and the name of one its properties:", function(){
+
+				describe("Matches if:", function(){
+
+					it("The given property is defined as readonly", function(){
+						var myObj = {};
+						Object.defineProperty(myObj, "name", {
+							enumerable: true,
+							configurable: false,
+							writable: false,
+							value: "Ciccio"
+						});
+						var result = matcher.compare(myObj, "name");
+						expect(result.pass).toBe(true);
+					});
+
+					it("The object is frozen", function(){
+						var myObj = {name: "Ciccio"};
+						Object.freeze(myObj);
+						var result = matcher.compare(myObj, "name");
+						expect(result.pass).toBe(true);
+					});
+
+				});
+
+				describe("Fails if:", function(){
+
+					it("The given property is not readonly", function(){
+						var result = matcher.compare({name: "Ciccio"}, "name");
+						expect(result.pass).toBe(false);
+						expect(result.message).toBe("Property: name is not readonly");
+					});
+
+					it("The given property does not exists", function(){
+						var result = matcher.compare({name: "Ciccio"}, "xxx");
+						expect(result.pass).toBe(false);
+						expect(result.message).toBe("Unable to find property: xxx");
+					});
+
+					it("The object is sealed but the property is still writeable", function(){
+						var myObj = {name: "Ciccio"};
+						Object.seal(myObj);
+						var result = matcher.compare(myObj, "name");
+						expect(result.pass).toBe(false);
+						expect(result.message).toBe("Property: name is not readonly");
+					});
+
+					it("The name of the property is not passed as a string", function(){
+						var result = matcher.compare({}, []);
+						expect(result.pass).toBe(false);
+						expect(result.message).toBe("Please specify the name of the property as string");
+					});
+
+					it("Only one argument is provided", function(){
+						var result = matcher.compare({});
+						expect(result.pass).toBe(false);
+						expect(result.message).toBe("Please specify the name of the property as string");
+					});
+
+				});
+
+			});
+
+		});
+
 	});
 
 });
